b135471e4dd8197db0532fb61eaef8ebf82fc1f9
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-01-18 15:13:36 -0800

    FEAT: To delete a specific object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        objs = FileStorage.__objects
+        if cls is None:
+            return objs
+        cls_objs = {key: obj for key, obj in objs.items() if type(obj) is cls}
+        return cls_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +52,11 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an obj from __objects"""
+        if obj is not None:
+            key = obj.to_dict()['__class__'] + "." + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
+                FileStorage.save(self)
---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines the test class for console. """
+import unittest
+from console import HBNBCommand
+
+
+class test_HBNBCommand(unittest.TestCase):
+    """ Defines test methods to test HBNBCommand class. """
+    pass
