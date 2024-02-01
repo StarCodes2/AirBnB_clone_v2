549aa6f5e6a424d7df9e7d7d9e6da3b9a12f1405
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-02-01 09:45:05 -0800

    BUG: pycodestyle indent fixed and -c flag added to compress archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,15 +6,14 @@ from fabric.api import local
 
 
 def do_pack():
-	""" Creates a tar archive of the directory web_static. """
-	if local("mkdir -p versions").failed is True:
-		return None
+    """ Creates a tar archive of the directory web_static. """
+    if local("mkdir -p versions").failed is True:
+        return None
 
-	d = datetime.utcnow()
-	arch = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year, d.month,
-							     d.day, d.hour,
-							     d.minute,
-							     d.second)
-	if local("tar -cvf {} web_static".format(arch)).failed is True:
-		return None
-	return arch
+    d = datetime.utcnow()
+    arch = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year, d.month,
+                                                         d.day, d.hour,
+                                                         d.minute, d.second)
+    if local("tar -cvzf {} web_static".format(arch)).failed is True:
+        return None
+    return arch
