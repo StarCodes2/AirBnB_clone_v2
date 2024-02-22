9076f78fdda93fe5669909c220d3541663955cdd
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-02-22 12:21:43 -0800

    Code Style Edit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello_bhnb():
     """ Display Hello HBNB! """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Displays HBNB """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
